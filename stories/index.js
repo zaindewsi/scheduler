efc38a6f0eaa8e3fd2f94e9a6c0df42e0eea6f95
Zain Dewsi <zain.dewsi@gmail.com>
2021-06-08 13:14:43 -0400

    Show appointment component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -137,7 +138,15 @@ storiesOf("DayList", module)
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
     .add("Header", () => <Header time="12pm" />)
-    .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => (
+      <Show
+        student="Lydia Miller-Jones"
+        interviewer={interviewer.name}
+        onEdit={action("onEdit")}
+        onDelete={action("onDelete")}
+      />
+    ));
 
 
     
