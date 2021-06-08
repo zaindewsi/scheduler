3d9c8ea74333009908522246368228d35dbf13db
Zain Dewsi <zain.dewsi@gmail.com>
2021-06-08 15:37:19 -0400

    display appointments/schedule

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -18,6 +19,45 @@ const days = [
     id: 3,
     name: "Wednesday",
     spots: 0,
+  },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "3pm",
+  },
+  {
+    id: 4,
+    time: "2pm",
+    interview: {
+      student: "Zain Dewsi",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
   },
 ];
 
@@ -43,7 +83,10 @@ export default function Application(props) {
         />{" "}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          return <Appointment key={appointment.id} {...appointment} />;
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import Empty from "./Empty";
+import Header from "./Header";
+import Show from "./Show";
 import "./styles.scss";
 
-export default function Appointment() {
-  return <article className="appointment"></article>;
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
