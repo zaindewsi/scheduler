586ad8752b470318ee0f4d169cc1d181ab52956d
Zain Dewsi <zain.dewsi@gmail.com>
2021-06-10 12:41:34 -0400

    refactor selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -6,10 +6,10 @@ export function getAppointmentsForDay(state, day) {
 }
 
 export function getInterview(state, interview) {
-  return interview
-    ? {
-        student: interview.student,
-        interviewer: { ...state.interviewers[interview.interviewer] },
-      }
-    : null;
+  return (
+    interview && {
+      student: interview.student,
+      interviewer: { ...state.interviewers[interview.interviewer] },
+    }
+  );
 }
