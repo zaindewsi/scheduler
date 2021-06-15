fcfcbd2dc4bb0036581a25881ec32cb8fb733ff5
Zain Dewsi <zain.dewsi@gmail.com>
2021-06-15 19:10:47 -0400

    breaking master with SET_DAY

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -31,27 +31,23 @@ export default function useApplicationData() {
       });
     });
 
-    try {
-      const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
 
-      webSocket.onopen = () => {
-        webSocket.send("ping");
+    webSocket.onopen = () => {
+      webSocket.send("ping");
 
-        webSocket.onmessage = event => {
-          const { type, id, interview } = JSON.parse(event.data);
+      webSocket.onmessage = event => {
+        const { type, id, interview } = JSON.parse(event.data);
 
-          if (type === "SET_INTERVIEW") {
-            dispatch({
-              type: type,
-              id: id,
-              interview: interview,
-            });
-          }
-        };
+        if (type === "SET_INTERVIEW") {
+          dispatch({
+            type: type,
+            id: id,
+            interview: interview,
+          });
+        }
       };
-    } catch (err) {
-      console.error(err);
-    }
+    };
   }, []);
 
   const bookInterview = (id, interview) => {
